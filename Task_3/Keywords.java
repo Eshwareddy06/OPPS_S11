dba7ab79fa4e5b9df01461a90817ed434d5252e6
Eshwar <eshwarreddy0989@gmail.com>
2025-08-01 11:43:35 +0530

    TASK-3

---
+++ Task_3/Keywords.java
@@ -0,0 +1,29 @@
+package Task_3;
+class Studentinfo {
+    String name;
+    long rollNumber;
+    String department;
+    Studentinfo(String name,long rollNumber,String department) {
+        this.name=name;
+        this.rollNumber=rollNumber;
+        this.department=department;
+    }
+    void showDetails() {
+        System.out.println("Name: " +this.name);
+        System.out.println("Roll Number: "+this.rollNumber);
+        System.out.println("Department: "+this.department);
+    }
+}
+
+public class Keywords {
+	public class Main {
+	      public static void main(String[] args) {
+	          Studentinfo s1 = new Studentinfo("Eshwar", 2420090096L, "CSIT");
+	          Studentinfo s2 = new Studentinfo("Varun ", 2420090097L, "CSE");
+
+	          s1.showDetails();
+	          s2.showDetails();
+	      }
+	  }
+
+}
