2c1dac566197160d019374d70c94e193d5b84589
Eshwar <eshwarreddy0989@gmail.com>
2025-08-01 11:42:21 +0530

    Add files via upload

---
+++ Practical/src/Task_1/Task_3/Objects.java
@@ -0,0 +1,28 @@
+package Task_3;
+class Details {
+    String name;
+    long rollNumber;
+    String department;
+    void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Department: " + department);
+    }
+}
+
+public class Objects {
+	public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name="Deepak";
+        s1.rollNumber=2420090L;
+        s1.department="CSIT";
+        s1.showDetails();
+        
+        Student s2=new Student();
+        s2.name="Eshwar";
+        s2.rollNumber=2420090153L;
+        s2.department="CSE";
+        s2.showDetails();
+    }
+
+}
