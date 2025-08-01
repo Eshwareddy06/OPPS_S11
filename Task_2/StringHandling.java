26da7a7792f42f0b92a8c87b31b2c67d6eb36ca0
Eshwar <eshwarreddy0989@gmail.com>
2025-08-01 11:32:36 +0530

    Task_2

---
+++ Task_2/StringHandling.java
@@ -0,0 +1,14 @@
+package Task_2;
+
+public class StringHandling {
+	public static void main(String[] args) {
+		String name="Eshwar";
+        System.out.println("Original String: "+name);
+        System.out.println("Length: "+name.length());
+        System.out.println("Uppercase: "+name.toUpperCase());
+        System.out.println("Lowercase: "+name.toLowerCase());
+        System.out.println("Character at index 3: "+name.charAt(1));
+        System.out.println("Substring(1to4): "+name.substring(1,4));
+	}
+	}
+		
